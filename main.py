611545ba4938a8f7ab5db32c4a0b4659ce7fb4a8
MichalRybecky <michalrybec@protonmail.com>
2020-12-31 14:58:37 +0100

    moved sound control to separate file in utils folder

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import random
 import pygame
 from utils import database
 from utils.load_assets import *
+from utils.sound_control import *
+
 
 WIDTH_H, HEIGHT_H = WIDTH // 2, HEIGHT // 2
 
@@ -226,31 +228,6 @@ class Enemy(Ship):
 
     def get_y(self):
         return self.y
-
-
-def music_on():
-    rng = random.randrange(4)
-    if rng == 0:
-        FORCE_THEME = pygame.mixer.music.load(os.path.join("assets/music", "force_theme.mp3"))
-        pygame.mixer.music.set_volume(1)
-    elif rng == 1:
-        ANAKINS_BETRAYAL = pygame.mixer.music.load(os.path.join("assets/music", "anakins_betrayal.mp3"))
-        pygame.mixer.music.set_volume(1)
-    elif rng == 2:
-        BATTLE_OF_THE_HEROES = pygame.mixer.music.load(os.path.join("assets/music", "battle_of_the_heroes.mp3"))
-        pygame.mixer.music.set_volume(5)
-    elif rng == 3:
-        DUEL_OF_THE_FATES = pygame.mixer.music.load(os.path.join("assets/music", "duel_of_the_fates.mp3"))
-        pygame.mixer.music.set_volume(0.6)
-    else:
-        JEDI_TEMPLE_MARCH = pygame.mixer.music.load(os.path.join("assets/music", "jedi_temple_march.mp3"))
-        pygame.mixer.music.set_volume(1)
-
-    pygame.mixer.music.play(-1)
-
-
-def music_off():
-    pygame.mixer.music.stop()
 
 
 def collide(obj1, obj2):
@@ -686,7 +663,7 @@ def change_ship_menu():
 
 
 if __name__ == '__main__':
-    music_playing = True
-    sfx_playing = True
+    # music_playing = True
+    # sfx_playing = True
     music_on()
     main_menu()
---
+++ utils/sound_control.py
@@ -0,0 +1,31 @@
+import os
+import random
+import pygame
+
+music_playing = True
+sfx_playing = True
+
+
+def music_on():
+    rng = random.randrange(4)
+    if rng == 0:
+        FORCE_THEME = pygame.mixer.music.load(os.path.join("assets/music", "force_theme.mp3"))
+        pygame.mixer.music.set_volume(1)
+    elif rng == 1:
+        ANAKINS_BETRAYAL = pygame.mixer.music.load(os.path.join("assets/music", "anakins_betrayal.mp3"))
+        pygame.mixer.music.set_volume(1)
+    elif rng == 2:
+        BATTLE_OF_THE_HEROES = pygame.mixer.music.load(os.path.join("assets/music", "battle_of_the_heroes.mp3"))
+        pygame.mixer.music.set_volume(5)
+    elif rng == 3:
+        DUEL_OF_THE_FATES = pygame.mixer.music.load(os.path.join("assets/music", "duel_of_the_fates.mp3"))
+        pygame.mixer.music.set_volume(0.6)
+    else:
+        JEDI_TEMPLE_MARCH = pygame.mixer.music.load(os.path.join("assets/music", "jedi_temple_march.mp3"))
+        pygame.mixer.music.set_volume(1)
+
+    pygame.mixer.music.play(-1)
+
+
+def music_off():
+    pygame.mixer.music.stop()
