865a9e2236ae60ea9880872b12f02cfd6c2019ee
MichalRybecky <michalrybec@protonmail.com>
2021-01-09 15:23:25 +0100

    reformatting with black

--- utils/database.py
+++ utils/database.py
@@ -7,12 +7,10 @@ date,playtime,score,wave
 
 from datetime import datetime
 
-stat_file = 'stats.txt'
+stat_file = "stats.txt"
 
 
 def add_game(score, wave, playtime):
     date = (str(datetime.now())).split()[0]
-    with open(stat_file, 'a') as file:
-        file.write(f'{date},{playtime},{score},{wave}\n')
-
-
+    with open(stat_file, "a") as file:
+        file.write(f"{date},{playtime},{score},{wave}\n")
--- utils/load_assets.py
+++ utils/load_assets.py
@@ -19,47 +19,49 @@ SFX_OFF = pygame.image.load(os.path.join("assets/menu_icons", "sfx_off.png"))
 SFX_OFF = pygame.transform.scale(SFX_OFF, (50, 50))
 
 # Loading Ship Images
-CIS_FIGHTER = pygame.image.load(
-    os.path.join("assets/skins", "cis_fighter.png"))
+CIS_FIGHTER = pygame.image.load(os.path.join("assets/skins", "cis_fighter.png"))
 CIS_HYENA_BOMBER = pygame.image.load(
-    os.path.join("assets/skins", "cis_hyena_bomber.png"))
+    os.path.join("assets/skins", "cis_hyena_bomber.png")
+)
 CIS_STRIKE_BOMBER = pygame.image.load(
-    os.path.join("assets/skins", "cis_strike_bomber.png"))
+    os.path.join("assets/skins", "cis_strike_bomber.png")
+)
 
 # Player Ships
-ARC_170 = pygame.image.load(
-    os.path.join("assets/skins", "arc_170.png"))
+ARC_170 = pygame.image.load(os.path.join("assets/skins", "arc_170.png"))
 ARC_170 = pygame.transform.scale(ARC_170, (150, 150))
-LAAT = pygame.image.load(
-    os.path.join("assets/skins", "laat.png"))
-Y_WING = pygame.image.load(
-    os.path.join("assets/skins", "y_wing.png"))
+LAAT = pygame.image.load(os.path.join("assets/skins", "laat.png"))
+Y_WING = pygame.image.load(os.path.join("assets/skins", "y_wing.png"))
 JEDI_INTERCEPTOR = pygame.image.load(
-    os.path.join("assets/skins", "jedi_interceptor.png"))
+    os.path.join("assets/skins", "jedi_interceptor.png")
+)
 
 # Lasers
 LASER_RED_SMALL = pygame.image.load(
-    os.path.join("assets/lasers", "laser_red_small.png"))
-LASER_RED_MED = pygame.image.load(
-    os.path.join("assets/lasers", "laser_red_med.png"))
-LASER_BLUE_MED = pygame.image.load(
-    os.path.join("assets/lasers", "laser_blue_med.png"))
+    os.path.join("assets/lasers", "laser_red_small.png")
+)
+LASER_RED_MED = pygame.image.load(os.path.join("assets/lasers", "laser_red_med.png"))
+LASER_BLUE_MED = pygame.image.load(os.path.join("assets/lasers", "laser_blue_med.png"))
 
 # Background
-BG = pygame.transform.scale(pygame.image.load(
-    os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+BG = pygame.transform.scale(
+    pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT)
+)
 
 # Power Ups
 SPEED_PU = pygame.image.load(os.path.join("assets/power_ups", "speed_pu.png"))
 HEALTH_PU = pygame.image.load(os.path.join("assets/power_ups", "health_pu.png"))
 LASER_SPEED_PU = pygame.image.load(
-    os.path.join("assets/power_ups", "laser_speed_pu.png"))
+    os.path.join("assets/power_ups", "laser_speed_pu.png")
+)
 ENEMY_FREEZE_PU = pygame.image.load(
-    os.path.join("assets/power_ups", "enemy_freeze_pu.png"))
+    os.path.join("assets/power_ups", "enemy_freeze_pu.png")
+)
 
 # SFX
 SFX_EXPLOSION_1 = pygame.mixer.Sound(os.path.join("assets/sfx", "explosion_1.wav"))
 SFX_PLAYER_FIRE = pygame.mixer.Sound(os.path.join("assets/sfx", "player_fire.wav"))
 SFX_ENEMY_FIRE = pygame.mixer.Sound(os.path.join("assets/sfx", "enemy_fire.wav"))
 SFX_PLAYER_DESTROYED = pygame.mixer.Sound(
-    os.path.join("assets/sfx", "player_explode.wav"))
+    os.path.join("assets/sfx", "player_explode.wav")
+)
