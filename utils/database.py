b69019d9d3b19871de9c3744bb443a2ba0dac2a4
MichalRybecky <michalrybec@protonmail.com>
2021-01-09 15:44:20 +0100

    added  file's function docstring

--- utils/database.py
+++ utils/database.py
@@ -7,10 +7,13 @@ date,playtime,score,wave
 
 from datetime import datetime
 
-stat_file = "stats.txt"
+STAT_FILE = "stats.txt"
 
 
 def add_game(score, wave, playtime):
+    """
+    function that adds a game into file in CSV format
+    """
     date = (str(datetime.now())).split()[0]
-    with open(stat_file, "a") as file:
+    with open(STAT_FILE, "a") as file:
         file.write(f"{date},{playtime},{score},{wave}\n")
