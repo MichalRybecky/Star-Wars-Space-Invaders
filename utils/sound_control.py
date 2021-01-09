11e15c3b280f1d3a240a7d37c27c837c4aa3e65f
MichalRybecky <michalrybec@protonmail.com>
2021-01-09 15:38:07 +0100

    added docstrings, rewrote function `music_on` to use dict to select music instead of (el)ifs, thus improved readability and efficiency

--- utils/sound_control.py
+++ utils/sound_control.py
@@ -1,3 +1,7 @@
+"""
+Module to control the sound effects / music
+"""
+
 import os
 import random
 import pygame
@@ -7,35 +11,28 @@ sfx_playing = True
 
 
 def music_on():
-    rng = random.randrange(4)
-    if rng == 0:
-        FORCE_THEME = pygame.mixer.music.load(
-            os.path.join("assets/music", "force_theme.mp3")
-        )
-        pygame.mixer.music.set_volume(1)
-    elif rng == 1:
-        ANAKINS_BETRAYAL = pygame.mixer.music.load(
-            os.path.join("assets/music", "anakins_betrayal.mp3")
-        )
-        pygame.mixer.music.set_volume(1)
-    elif rng == 2:
-        BATTLE_OF_THE_HEROES = pygame.mixer.music.load(
-            os.path.join("assets/music", "battle_of_the_heroes.mp3")
-        )
-        pygame.mixer.music.set_volume(5)
-    elif rng == 3:
-        DUEL_OF_THE_FATES = pygame.mixer.music.load(
-            os.path.join("assets/music", "duel_of_the_fates.mp3")
-        )
-        pygame.mixer.music.set_volume(0.6)
-    else:
-        JEDI_TEMPLE_MARCH = pygame.mixer.music.load(
-            os.path.join("assets/music", "jedi_temple_march.mp3")
-        )
-        pygame.mixer.music.set_volume(1)
+    """
+    picks random song and turns it ON
+    """
+
+    # dict of avaible songs to choose, key is ID to list with filename and it's volume level
+    songs = {
+        0: ["force_theme.mp3", 1],
+        1: ["anakins_betrayal.mp3", 1],
+        2: ["battle_of_the_heroes.mp3", 5],
+        3: ["duel of the fates.mp3", 0.6],
+        4: ["]jedi_temple_march.mp3", 1],
+    }
+
+    SELECTED_SONG = songs[random.randrange(4)]
+    pygame.mixer.music.load(os.path.join("assets/music", SELECTED_SONG[0]))
+    pygame.mixer.music.set_volume(SELECTED_SONG[1])
 
     pygame.mixer.music.play(-1)
 
 
 def music_off():
+    """
+    turns pygame music OFF
+    """
     pygame.mixer.music.stop()
